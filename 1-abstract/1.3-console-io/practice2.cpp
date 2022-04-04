7131ccda6cb2ce99d08cd777c58186b86d5d24da
Lfu001 <goodspeed913@gmail.com>
2022-04-04 17:14:58 +0900

    Add 1-abstract/1.3-console-io/practice2.cpp

---
+++ 1-abstract/1.3-console-io/practice2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    const char* name = "Murabito";
+    int age = 17;
+    double weight = 46.3;
+
+    cout << name << endl;
+    cout << age << endl;
+    cout << weight << endl;
+}

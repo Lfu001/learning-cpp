c83b7c88a83cae35e22323783723a3b18b3b0b80
Lfu001 <goodspeed913@gmail.com>
2022-04-04 17:23:23 +0900

    Add 1-abstract/1.3-console-io/practice3.cpp

---
+++ 1-abstract/1.3-console-io/practice3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    const char* name = "Murabito";
+    int age = 17;
+    double weight = 46.3;
+
+    cout << "Hi, my name is " << name << ". I'm " << age
+         << " years old and weigh " << weight << " kg." << endl;
+}

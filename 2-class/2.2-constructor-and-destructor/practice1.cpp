8f8404f2cf680687de0b38021ddc9ef780c2a5df
Lfu001 <goodspeed913@gmail.com>
2022-04-05 23:23:17 +0900

    Add 2-class/2.2-constructor-and-destructor/practice1.cpp

---
+++ 2-class/2.2-constructor-and-destructor/practice1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+class A {
+    int a;
+
+ public:
+    A() {
+        cout << "Constructor A() called." << endl;
+        // a = 0;    // a == 1 before initialized.
+        a = 0;
+    }
+    void print() {
+        cout << a << endl;
+    }
+};
+
+int main() {
+    A x;
+    x.print();
+}
